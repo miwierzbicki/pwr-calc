98264eb05ab03281e9c258297c54d5f57312b7a1
ufolud <m.wierzbicki7@outlook.com>
2021-10-21 13:41:16 +0200

    reworked code, new ui and functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QtMath>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +11,17 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    double Xa = ui->spinBoxXa->value();
+    double Ya = ui->spinBoxYa->value();
+    double Xb = ui->spinBoxXb->value();
+    double Yb = ui->spinBoxYb->value();
+    double vectorLength = qSqrt(qPow(Xb-Xa, 2)+qPow(Yb-Ya, 2)); //z libki qtmath
+    QString labelVectorLength = QString::number(vectorLength, 'f', 3);
+    ui->labelVectorLength->setText(labelVectorLength);
 }
 
