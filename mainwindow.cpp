e56bdf02d3b04fdfb079a97e6323e8610e1598d6
ufolud <m.wierzbicki7@outlook.com>
2021-10-22 15:12:38 +0200

    refactored method names

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->plot->addGraph();
+    ui->plot->xAxis->setLabel("x"); //generowanie osi x,y w konstruktorze
+    ui->plot->yAxis->setLabel("y");
 }
 
 MainWindow::~MainWindow()
@@ -14,7 +17,7 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::calculateVectorBtn()
 {
     double Xa = ui->spinBoxXa->value();
     double Ya = ui->spinBoxYa->value();
@@ -27,27 +30,26 @@ void MainWindow::on_pushButton_clicked()
 
 
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::generatePlotBtn()
 {
     QVector<double> x(101), y(101);
     for(int i=0; i<101; ++i) {
         x[i] = i/50.0-1;
         y[i] = x[i]*x[i];
     }
-    ui->plot->addGraph();
-    ui->plot->graph(0)->setData(x,y);
-    ui->plot->xAxis->setLabel("x");
-    ui->plot->yAxis->setLabel("y");
+    //ui->plot->addGraph(); idk czy tu tez musze dodawac, w razie czego jest replot()
+    ui->plot->graph(0)->setData(x,y); //z dokumentacji, testowy wykres
+    double axisRange = ui->axisSpinBox->value();
     ui->plot->xAxis->setRange(-1,1);
-    ui->plot->yAxis->setRange(0,1);
+    ui->plot->yAxis->setRange(0,axisRange);
     ui->plot->replot();
 }
 
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::savePlotBtn()
 {
-    QString filename = "plot.png";
-    QString outputDir = "C:/Users/Mirek/Desktop/pwr-calc";
+    QString filename = "plot.png"; //dodac moze wzor do nazwy pliku idk
+    QString outputDir = "C:/Users/Mirek/Desktop/pwr-calc"; //zmienic na dialog z wyborem miejsca zapisu
     ui->plot->savePng(outputDir+"/"+filename, 500, 400);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -16,11 +16,11 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void calculateVectorBtn();
 
-    void on_pushButton_2_clicked();
+    void generatePlotBtn();
 
-    void on_pushButton_3_clicked();
+    void savePlotBtn();
 
 private:
     Ui::MainWindow *ui;
